55166ebb6b269859e06c5bdc883bf82cf1afb479
Hemsas Riane <rayan.hemsas@gmail.com>
2023-07-30 19:53:01 +0200

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+
+    function divide (a,b){
+        return a/b;
+
+    }
+divide(2000,100);
+const square = parameter => {
+    return parameter*parameter;
+}
+// console.log(square(5));
+
+const add = (parameter1,parameter2) => {
+    return parameter1+parameter2;
+}
+// console.log(add(78));
